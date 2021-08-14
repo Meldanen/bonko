95cef1f50e14876fd43c595cad8c57c2784ce36a
Meldanen <amegalemos1@gmail.com>
2021-08-14 16:28:26 +0300

    Update LoopService.py

--- services/LoopService.py
+++ services/LoopService.py
@@ -105,7 +105,7 @@ class LoopService:
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
             self.logging_service.log(f'Attempting daily {CommandsEnum.WORD_OF_THE_DAY.value}')
-            # await self.handle_daily_word_of_the_day()
+            await self.handle_daily_word_of_the_day()
             await self.handle_daily_siblings_sibling_penor()
             await asyncio.sleep(60 * 55)  # wait 55 minutes
 
@@ -118,7 +118,7 @@ class LoopService:
             guilds = self.bot.guilds
             for guild in guilds:
                 for channel in guild.text_channels:
-                    if channel.name == "general":
+                    if channel.name == "discord-games":
                         self.logging_service.log_starting_progress(CommandsEnum.WORD_OF_THE_DAY.value)
                         emoji = await EmojiEnum.get_custom_emoji(channel.guild.emojis, EmojiEnum.BONK.value)
                         message = await channel.send("Word of the day: bonk")
